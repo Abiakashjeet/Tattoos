4588ccff81003b08314c03da878578143ee7a763
Abiakashjeet Singh <Sidhuavi@icloud.com>
2020-12-12 05:38:36 -0500

    retrofit post data

--- app/src/main/java/com/example/tattoos/MainActivity.java
+++ app/src/main/java/com/example/tattoos/MainActivity.java
@@ -17,7 +17,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
    EditText Email, Password;
-   Button Login, Registration, Text_Result;
+   Button Login, Registration, Text_Result, Text2Result;
 
     FirebaseAuth fAuth;
 
@@ -36,11 +36,21 @@ public class MainActivity extends AppCompatActivity {
         Login = (Button) findViewById(R.id.login);
         Registration = (Button) findViewById(R.id.registration);
         Text_Result=(Button)findViewById(R.id.text_result);
+        Text2Result=(Button)findViewById(R.id.text2result);
 
         Text_Result.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(MainActivity.this, Data.class);
+
+                startActivity(intent);
+            }
+        });
+
+        Text2Result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,PData.class);
 
                 startActivity(intent);
             }
--- app/src/main/java/com/example/tattoos/PData.java
+++ app/src/main/java/com/example/tattoos/PData.java
@@ -8,59 +8,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.tattoos.api_interfaces.JsonPlaceHolderApi;
 import com.example.tattoos.models.Post;
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class Data extends AppCompatActivity {
+public class PData extends AppCompatActivity {
 
     private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_data);
+        setContentView(R.layout.activity_p_data);
 
         textViewResult = findViewById(R.id.text_view_result);
 
-        Retrofit retrofit=new Retrofit.Builder()
+        createPost();
+    }
+
+    void createPost() {
+
+        Post post = new Post("shivam", "sharma", "shivam@gmail.com", "qwertyuiop");
+        Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://my-json-server.typicode.com/Abiakashjeet/mockjson/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-
         JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
-        Call<List<Post>> call=jsonPlaceHolderApi.getPosts();
+        Call<Post> call = jsonPlaceHolderApi.createPost(post);
 
-
-        call.enqueue(new Callback<List<Post>>() {
+        call.enqueue(new Callback<Post>() {
             @Override
-            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-                if (!response.isSuccessful()) {
-                    textViewResult.setText("Code: "+response.code());
-                    return;
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                if (!response.isSuccessful()){
+                    textViewResult.setText("Code: " +response.code());
                 }
+                Post postresponse=response.body();
+                String content = "";
+                content += "Fname: "+post.getEmail() + "\n";
+                content += "Lname: "+post.getEmail() + "\n";
+                content += "Email: "+post.getEmail() + "\n";
+                content += "Password"+post.getPassword() + "\n";
 
-                List<Post> posts = response.body();
-                for(Post post:posts){
-                    String content = "";
-                    content += "Fname: "+post.getEmail() + "\n";
-                    content += "Lname: "+post.getEmail() + "\n";
-                    content += "Email: "+post.getEmail() + "\n";
-                    content += "Password"+post.getPassword() + "\n";
+                textViewResult.append(content);
 
-                    textViewResult.append(content);
-                }
             }
 
             @Override
-            public void onFailure(Call<List<Post>> call, Throwable t) {
+            public void onFailure(Call<Post> call, Throwable t) {
                 textViewResult.setText(t.getMessage());
             }
         });
+
 
     }
 }
